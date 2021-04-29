42e46d04d53845f74eab1978cfa1301cbb59fd8b
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2021-04-29 09:04:29 +0300

    Update Prints.py

--- Prints.py
+++ Prints.py
@@ -12,11 +12,11 @@ def PrintRules():
           "\n\tТебе нужно продержаться на столько долго, на сколько это возможно, поэтому принимай решения быстро,"
           "\n\tчтобы избежать все астероиды на пути \n\t Кстати, у вашего экипажа, по-моему, отказали реактивные тормоза,"
           "\n\tПоэтому будь сконцентрирован и знай, что вы набираете скорость."
-          "\n\tПусть путешествие вам понравится и удачи с исследованием космоса в 'Asteroids Game!'")
+          "\n\tПусть путешествие вам понравится и удачи с исследованием космоса в 'Asteroid Crash!'")
     print("ENG:")
     print("\tHi there, buddy, that's your next space exploration mission. Try your best and explore as much outer"
           "\n\tspace as possible, but be aware of the asteroids that can damage yor spaceship on the way."
           "\n\tYou have to try to last as much as you even can and think fast to avoid the asteroids along the way"
           "\n\tBy the way, your team got a problem with jet brakes so you will pick up speed so be focused and aware."
-          "\n\tHave an incredible journey and good luck with your exploration of the 'Asteroids Game!'")
+          "\n\tHave an incredible journey and good luck with your exploration of the 'Asteroid Crash!'")
     print("----------------------------------------------------------------------------------")
