4efe3b24b1eddf6eeae8665c3d9d7785f67b15a3
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2021-04-29 08:45:57 +0300

    Add files via upload

--- Prints.py
+++ Prints.py
@@ -1,16 +1,22 @@
+
 def PrintScore(score):
     print("YOUR SCORE:" + "\n" + "➤ " + str(score) + " m")
     print("-" * (5 + len(str(score))))
 
 
 def PrintRules():
-    print("RULES ARE:")
+    print(" THE RULES ARE:")
     print("RUS:")
     print("\tПривет, юный космонавт, это твоя очередная миссия по изучению космоса. Попробуй исследовать как можно"
           " больше\n\tкосмического пространства, но остерегайся астероидов, которые будут встречаться тебя на пути."
-          "\n\tКстати, у вашего экипажа, по-моему, отказали реактивные тормоза, поэтому вы будете набирать скорость.")
+          "\n\tТебе нужно продержаться на столько долго, на сколько это возможно, поэтому принимай решения быстро,"
+          "\n\tчтобы избежать все астероиды на пути \n\t Кстати, у вашего экипажа, по-моему, отказали реактивные тормоза,"
+          "\n\tПоэтому будь сконцентрирован и знай, что вы набираете скорость."
+          "\n\tПусть путешествие вам понравится и удачи с исследованием космоса в 'Asteroids Game!'")
     print("ENG:")
-    print("\tHi there, buddy, that's your next space exploration mission. Try your best and explore as much space"
-          "\n\touter as possible, but beware of asteroids that can damage yor spaceship on the way."
-          "\n\tBy the way, your team got a problem with jet brakes so you will pick up speed.")
+    print("\tHi there, buddy, that's your next space exploration mission. Try your best and explore as much outer"
+          "\n\tspace as possible, but be aware of the asteroids that can damage yor spaceship on the way."
+          "\n\tYou have to try to last as much as you even can and think fast to avoid the asteroids along the way"
+          "\n\tBy the way, your team got a problem with jet brakes so you will pick up speed so be focused and aware."
+          "\n\tHave an incredible journey and good luck with your exploration of the 'Asteroids Game!'")
     print("----------------------------------------------------------------------------------")
